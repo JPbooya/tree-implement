90c0e1ce04bc681021027dd1f5a40e4149c8ab36
Johncarlo Perez <johncarloperez@Johncarlos-MacBook-Pro.local>
2025-05-21 11:56:18 -0700

    finished in class assignemnt

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,27 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
+    
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(15);
+
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+    root.left.left = new TreeNode(-3);
+    root.right.right = new TreeNode(5);
+
+    root.right.right.right = new TreeNode(22);
+
+    preOrder(root);
 
   }
+
+  public static void preOrder (TreeNode current) {
+    if(current == null) return;
+    System.out.println(current.data);
+    preOrder(current.left);
+    preOrder(current.right);
+  }
+
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,11 @@
-public class TreeNode {
+import java.lang.classfile.components.ClassPrinter.ListNode;
 
-  
+public class TreeNode {
+    public int data;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode (int data) {
+        this.data = data;
+    }
 }
